7ef4b760d54698d0cb4b9541e2e4c871eb3405ac
Ali Bennaser <alibennaserofficial@gmail.com>
2024-10-16 23:21:49 +0200

    4353

---
+++ Week_2/Day_2/Core/function.js
@@ -0,0 +1,21 @@
+
+
+ 
+
+function changetext(element){
+ if(element.innerHTML == "login"){
+
+    element.innerHTML = "logout" 
+ }else {
+    element.innerHTML = "login"
+ }
+}
+
+function removeit(element){
+    element.remove() 
+  }
+
+  function aler6t(){
+alert("ninja was liked")
+
+}
